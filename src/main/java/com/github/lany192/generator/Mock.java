3b47896825aa1f16b8a428e55592f4ec748571dc
lany192 <lany192@qq.com>
2021-07-21 17:51:56 +0800

    Update

--- src/main/java/com/github/lany192/generator/Mock.java
+++ src/main/java/com/github/lany192/generator/Mock.java
@@ -7,13 +7,15 @@ public class Mock {
 
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
+            int sex = Integer.parseInt(new SexGenerator().build());
             User user = new User();
+            user.setUsername(new ChineseNameGenerator(sex == 1).build());
             user.setNickname(new NicknameGenerator().build());
             user.setBirthday(new BirthdayGenerator().getBirthday().toString());
             user.setAvatar(new AvatarGenerator().build());
             user.setIdCard(new IDCardGenerator().build());
             user.setEmail(new EmailGenerator(5, 9).build());
-            user.setSex(Integer.parseInt(new SexGenerator().build()));
+            user.setSex(sex);
             user.setPhone(new PhoneGenerator().build());
             user.setSignature(new SignatureGenerator().build());
             System.out.println("模拟用户:" + JsonUtils.object2json(user));
