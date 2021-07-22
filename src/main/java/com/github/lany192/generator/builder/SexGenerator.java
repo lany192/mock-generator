4c2612e0ddbc3c9fc6afb8d69ed04c4bcdddf2e6
lany192 <lany192@qq.com>
2021-07-22 10:38:48 +0800

    Update

--- src/main/java/com/github/lany192/generator/builder/SexGenerator.java
+++ src/main/java/com/github/lany192/generator/builder/SexGenerator.java
@@ -3,26 +3,19 @@ package com.github.lany192.generator.builder;
 import org.apache.commons.lang3.RandomUtils;
 
 /**
- * 0表示女，1表示男
+ * 0表示未知，1表示男，2表示女
  */
 public class SexGenerator implements Generator {
-    private int sex = -1;
-
-    private void random() {
-        sex = RandomUtils.nextInt(0, 2);
-    }
+    private int sex;
 
     public boolean isBoy() {
-        if (sex == -1) {
-            random();
-        }
         return sex == 1;
     }
 
     @Override
     public Integer build() {
-        if (sex == -1) {
-            random();
+        if (sex == 0) {
+            sex = RandomUtils.nextInt(0, 3);
         }
         return sex;
     }
