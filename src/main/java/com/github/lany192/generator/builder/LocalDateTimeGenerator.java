30a0b8c6e76714514da45179b758773df8c947d8
lany192 <lany192@qq.com>
2022-12-14 16:22:42 +0800

    update

---
+++ src/main/java/com/github/lany192/generator/builder/LocalDateTimeGenerator.java
@@ -0,0 +1,37 @@
+package com.github.lany192.generator.builder;
+
+import org.apache.commons.lang3.RandomUtils;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+
+public class LocalDateTimeGenerator implements Generator<LocalDateTime> {
+    private final LocalDateTime start;
+    private final LocalDateTime end;
+
+    public LocalDateTimeGenerator(LocalDateTime start, LocalDateTime end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public LocalDateTime build() {
+        int year = RandomUtils.nextInt(start.getYear(), end.getYear());
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, RandomUtils.nextInt(1, 12));
+        calendar.set(Calendar.DATE, RandomUtils.nextInt(1, 31));
+        calendar.set(Calendar.HOUR, RandomUtils.nextInt(0, 23));
+        calendar.set(Calendar.MINUTE, RandomUtils.nextInt(0, 59));
+        calendar.set(Calendar.SECOND, RandomUtils.nextInt(0, 59));
+
+        LocalDateTime result = calendar.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        //如果比start小，重新生成
+        if (result.isAfter(start) && result.isBefore(end)) {
+            return result;
+        } else {
+            return build();
+        }
+    }
+}
